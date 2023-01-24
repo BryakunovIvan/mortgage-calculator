c3b2ae38d23e461bb3e7882f35b62ce2b9a50c8e
Ivan Bryakunov <ivan.bryakunov@gmail.com>
2023-01-24 21:23:17 +0400

    Написал TODO

--- src/components/calculator/components/calculator-field/models.ts
+++ src/components/calculator/components/calculator-field/models.ts
@@ -1,3 +1,4 @@
 import { ICalculatorMainValues } from "../../context";
 
+// TODO: Сузить типы fieldName до суммы кредита и первоначального взноса
 export type TProps = { fieldName: keyof ICalculatorMainValues };
